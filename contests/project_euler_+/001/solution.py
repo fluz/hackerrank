fbe16d3b102510da7b0065fa24add58c580441e1
fernando_luz <fernando_luz@tpn.usp.br>
2016-02-01 08:20:15 -0200

    read input in project euler+ #1

---
+++ contests/project_euler_+/001/solution.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    n_cases = int(raw_input())
+    for i in xrange(n_cases):
+        case = int(raw_input())
+        # print "{0}".format(sum_three_five(case))
+    return 0
+
+if __name__ == "__main__":
+    main()
