986c6efc67fe00abb1c2d98591eceeb089ee902e
fernando_luz <fernando_luz@tpn.usp.br>
2016-02-01 16:03:30 -0200

    solving the pe+1

--- contests/project_euler_+/001/solution.py
+++ contests/project_euler_+/001/solution.py
@@ -1,10 +1,24 @@
+def sum_multiples_of_three_and_five(number):
+    sum_3 = (3 + ((number - 1) // 3) * 3)
+    sum_3 *= (number - 1) // 3
+    sum_3 /= 2
+    sum_5 = (5 + ((number - 1) // 5) * 5)
+    sum_5 *= (number - 1) // 5
+    sum_5 /= 2
+    sum_15 = (15 + ((number - 1) // 15) * 15)
+    sum_15 *= (number - 1) // 15
+    sum_15 /= 2
+
+    total = sum_3 + sum_5 - sum_15
+
+    return total
 
 
 def main():
     n_cases = int(raw_input())
     for i in xrange(n_cases):
         case = int(raw_input())
-        # print "{0}".format(sum_three_five(case))
+        print "{0}".format(sum_multiples_of_three_and_five(case))
     return 0
 
 if __name__ == "__main__":
---
+++ contests/project_euler_+/001/test_solution.py
@@ -0,0 +1,16 @@
+import unittest
+import solution
+from os import listdir
+
+from ddt import ddt, file_data
+
+
+@ddt
+class TddProjectEuler001(unittest.TestCase):
+
+    @file_data("dataset2test.json")
+    def test_case(self, input, output):
+        n_cases = input.pop(0)
+        for i in xrange(n_cases):
+            result = solution.sum_multiples_of_three_and_five(input[i])
+            self.assertEqual(result, output[i])
